4ef4c797d6359d49d76da6c5b6e0c5844a447a8a
nitemi@ <blessedtgirl@gmail.com>
2024-01-30 12:20:27 +0100

    Add project files.

---
+++ Inheritance/Higher Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class HigherEmployee : Lower_Employee
+    {
+
+        public void bonus()
+        {
+           
+           int bonus = 13000;
+
+            Console.WriteLine("The higher employer bonus per extra workdone is:", bonus);
+            
+        }
+    }
+}
---
+++ Inheritance/Lower Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Lower_Employee
+    {
+        public string fullName;
+            public int age;
+            public string maritalstatus;
+        public void info(string fullName, int age, string maritalstatus)
+        {
+            string fullName = fullName;
+            int age = age;
+            string maritalstatus = maritalstatus;
+            
+
+            Console.WriteLine("Information about the employee:");
+            Console.WriteLine("Fullname of Employee:" + fullName);
+            Console.WriteLine("Age:" + age);
+            Console.WriteLine("Marital Status:" + maritalstatus);
+
+
+        }
+        public void salary(int salaryamount) 
+        {
+           int salaryamount = salaryamount;
+            Console.WriteLine("The default annual amount for a lower employee is {0}", salaryamount*12);
+        }
+    }
+}
